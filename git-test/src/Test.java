1d5293446a6c9798fb7ca8b4e6417a3142bed185
slask593 <Sergei.Laskov@marriott-sp.com>
2015-10-18 22:42:49 -0400

    Change to Test and new Test2
    Signed-off-by: slask593 <Sergei.Laskov@marriott-sp.com>

--- git-test/src/Test.java
+++ git-test/src/Test.java
@@ -2,8 +2,5 @@
 public class Test {
 	public static void main(String[] args) {
 		System.out.println(">>>>>>>>>>>>");
-		System.out.println(">>>>>>>>>>>> remote");
-		System.out.println(">>>>>>>>>>>> remote2");
-		System.out.println(">>>>>>>>>>>> local");
 	}
 }
