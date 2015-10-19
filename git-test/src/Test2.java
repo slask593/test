2130a25648bfdf700eefddb8256a3843cb3938b7
slask593 <Sergei.Laskov@marriott-sp.com>
2015-10-19 08:28:26 -0400

    Added local 3

--- git-test/src/Test2.java
+++ git-test/src/Test2.java
@@ -3,5 +3,6 @@ public class Test2 {
 	public static void main(String[] args) {
 		System.out.println(">>>>>>>>>>>> local");
 		System.out.println(">>>>>>>>>>>> local 2");
+		System.out.println(">>>>>>>>>>>> local 3");
 	}
 }
